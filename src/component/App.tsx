88a1d85afe0e9b5cbfeee4550167568c6f914151
gureuso <wyun13043@gmail.com>
2019-12-23 16:09:38 +0900

    Add absolute path

---
+++ src/component/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'static/css/App.less';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
